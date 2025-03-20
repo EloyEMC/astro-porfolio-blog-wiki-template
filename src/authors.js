3f06bdd72d32858e1408be871dfc9e0fb4ab904c
EloyEMC <eloy@eloymartinezcuesta.com>
2025-03-20 19:29:38 +0100

    Schema 2

---
+++ src/authors.js
@@ -0,0 +1,24 @@
+/** @type {import('../types/authors').Authors} */
+export const authors = {
+    "eloymartinez": {
+    name: "Eloy Martínez",
+    image: "/eloy_feria_iluminacion.webp",
+    bio: "Desarrollador web y móvil. Siempre aprendiendo.",
+    social: {
+      twitter: "https://twitter.com/eloy_emc",
+      github: "https://github.com/EloyEMC",
+      linkedin: "https://www.linkedin.com/in/eloymartinezemc/",
+      youtube: "https://youtube.com/@eloymartinez"
+      }
+    },
+    "janedoe": {
+      name: "Jane Doe",
+      image: "/images/authors/janedoe.jpg",
+      bio: "Diseñadora y desarrolladora frontend con amor por la accesibilidad.",
+      social: {
+        twitter: "https://twitter.com/janedoe",
+        github: "https://github.com/janedoe"
+      }
+    }
+  };
+  
